29eb89f0ba93922d9bb1c1b6929b7d3a81a1eaec
unknown <lucasmendoncawf@gmail.com>
2025-04-07 21:04:57 -0300

    first commit

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuth, getToken } from "./authService";
 
 const api = axios.create({
-  baseURL: "http://localhost:8888/.netlify/functions/",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use(
